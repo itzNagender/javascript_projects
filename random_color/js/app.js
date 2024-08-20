5d550bd596294d57d1c91ae1765c662dbcfe9d73
itznagender <kumarnagender811@gmail.com>
2024-08-20 21:07:15 +0530

    Random Color Generator

---
+++ random_color/js/app.js
@@ -0,0 +1,19 @@
+const getColor = () => {
+    //hexcode
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16)
+    document.body.style.backgroundColor = randomCode
+    document.getElementById("color-code").innerText = randomCode;
+
+    //to copy in clipboard automatically
+    navigator.clipboard.writeText(randomCode);
+}
+
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+//init call
+getColor()
